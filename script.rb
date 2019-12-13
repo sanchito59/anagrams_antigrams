4803aa425c62de553009b15aa09bfbe93b5d0187
Chris Sanchez <c.sanch7@gmail.com>
2019-12-13 10:34:01 -0800

    update UI

--- script.rb
+++ script.rb
@@ -1,6 +1,16 @@
 #!/usr/bin/env ruby
 require('./lib/anagrams.rb')
 
-# puts "__________________"
-# puts " "
-# puts "Enter a word: "
+puts "
+- ANAGRAM DETECTOR - 
+"
+puts "____________________"
+puts " "
+puts "Enter a word: "
+word1 = gets.chomp
+word1 = Word.new(word1)
+puts "Enter another word: "
+word2 = gets.chomp
+puts "____________________"
+puts word1.anagram_detector(word2)
+puts "____________________"
