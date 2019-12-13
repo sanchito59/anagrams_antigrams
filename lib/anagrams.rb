06bd95868f371872bbbd4932f88eb36482795d09
Chris Sanchez <c.sanch7@gmail.com>
2019-12-13 09:48:26 -0800

    add ability to compare different words

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -3,10 +3,28 @@ class Word
     @word = str
   end
 
-  def anagram_detector
-    @word.downcase().split('').sort().join('')
+  def word2(str2)
+    @word2 = str2
+  end
+
+  def anagram_detector(word2)
+    puts word2 + "detc"
+    word1 = @word.downcase().split('').sort().join('')
+    puts word1 + " TEST TEST"
+  end
+
+  def anagram?(word2)
+    puts word2 + "an?"
+    if 'eat' == 'eat'
+      puts true
+    else 
+      puts false
+    end
   end
 end
 
-# word1 = Word.new("hippo")
-# puts word1.anagram_detector
+word1 = gets.chomp
+word1 = Word.new(word1)
+word2 = gets.chomp
+puts word1.anagram_detector(word2)
+puts word1.anagram?(word2)
--- script.rb
+++ script.rb
@@ -1,2 +1,6 @@
 #!/usr/bin/env ruby
 require('./lib/anagrams.rb')
+
+# puts "__________________"
+# puts " "
+# puts "Enter a word: "
