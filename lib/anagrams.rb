86628838f6cc9a1060d7bc851121128a999499c9
Chris Sanchez <c.sanch7@gmail.com>
2019-12-13 14:58:46 -0800

    add passing test for creating a new hash based on letter occurence

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -40,13 +40,34 @@ class Word
         "That wasn't a word!"
       end
    end
+   
+   def antigrams_hash(arr)
+    hash = Hash.new(0)
+    arr.each_with_object(Hash.new(0)){|key, hash| hash[key] += 1}
+   end
 
   def anagram_detector(word2)
+    anti1 = @word.split('')
+    anti2 = word2.downcase().gsub(@special_char, "").split('')
+    word1_char_count = antigrams_hash(anti1)
+    word2_char_count = antigrams_hash(anti2)
+    puts antigrams_hash(['t','e','a'])
+
     word2 = word2.downcase().gsub(@special_char, "").split('').sort().join('')
     if @word == word2
       "Anagram match!"
     else 
       "Not an anagram!"
+    end
+    puts word1_char_count
+    puts word2_char_count
+    puts " --- "
+    puts " --- "
+    puts word1_char_count.keys()
+    puts " --- "
+    puts word2_char_count.keys()
+    if word1_char_count == word2_char_count
+      puts "Not a single letter matches!"
     end
   end
 end
