d5b136a53c0a90b57a675424166d781cd47575c0
Chris Sanchez <c.sanch7@gmail.com>
2019-12-13 14:53:20 -0800

    add failing test for creating a hash based on letter occurence

--- spec/ruby_file_spec.rb
+++ spec/ruby_file_spec.rb
@@ -19,19 +19,24 @@ describe('#anagram_detector') do
     #     word = Word.new("HiPpO")
     #     expect(word.anagram_detector()).to(eq('hiopp'))
     # end
-    it('will detect whether a phrase or word is an anagram') do
-        word = Word.new("CLint EASTwoOD")
-        word2 = "OLD WEST ACTION"
-        expect(word.anagram_detector(word2)).to(eq("Anagram match!"))
-    end
-    it('will detect whether the inputs are words') do
-        word = Word.new('mjk')
-        word2 = "hello"
-        expect(word.vowel_check(word2)).to(eq("That wasn't a word!"))
-    end
-    it('will detect whether two words are anagrams or not') do
+    # it('will detect whether a phrase or word is an anagram') do
+    #     word = Word.new("CLint EASTwoOD")
+    #     word2 = "OLD WEST ACTION"
+    #     expect(word.anagram_detector(word2)).to(eq("Anagram match!"))
+    # end
+    # it('will detect whether the inputs are words') do
+    #     word = Word.new('mjk')
+    #     word2 = "hello"
+    #     expect(word.vowel_check(word2)).to(eq("That wasn't a word!"))
+    # end
+    # it('will detect whether two words are anagrams or not') do
+    #     word = Word.new('tEa')
+    #     word2 = "EAT"
+    #     expect(word.anagram_detector(word2)).to(eq("Anagram match!"))
+    # end
+    it('will create a new hash based on letter occurence') do
         word = Word.new('tEa')
-        word2 = "EAT"
-        expect(word.anagram_detector(word2)).to(eq("Anagram match!"))
+        arr = ['t', 'e', 'a']
+        expect(word.antigrams_hash(arr)).to(eq({"t"=>1, "e"=>1, "a"=>1}))
     end
 end
