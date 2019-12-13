bd2f8945a858ccd972111b485dd76b588b9132e1
Chris Sanchez <c.sanch7@gmail.com>
2019-12-13 12:12:43 -0800

    add WIP code for checking vowels/ if a word is a word

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,12 +1,40 @@
 class Word
   def initialize(str)
-    @word = str
+    @word = str.downcase().gsub(" ", "").split('').sort().join('')
+    @vowels = {
+      'a' => /[a]/,
+      'e' => /[e]/,
+      'i' => /[i]/,
+      'o' => /[o]/,
+      'u' => /[u]/,
+      'y' => /[y]/
+    }
   end
+  
+    def vowel_check(word2)
+      word1 = @word.split('')
+      puts word1
+      puts " "
+      word2 = word2.split('')
+      puts word2
+      puts " "
+      counter = 0 
+      word1.each do |letter |
+        @vowels.each do |key, value|
+          if letter =~ value
+            counter +=1
+            # else
+            #   puts "Word1 does not contain #{value}"
+          end
+        end
+      end
+      puts counter
+   end
 
   def anagram_detector(word2)
-    word1 = @word.downcase().gsub(" ", "").split('').sort().join('')
     word2 = word2.downcase().gsub(" ", "").split('').sort().join('')
-    if word1 == word2
+    vowel_check(word2)
+    if @word == word2
       puts "Anagram match!"
       true
     else 
